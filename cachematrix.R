96ff24e3c130d1dd33984c05541b99794f574b19
Enoch <enoches@gmail.com>
2014-05-25 19:44:15 -0300

    First step, just testing repo creation

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
+####### first step, just testing repository creation...
+
 ## Put comments here that give an overall description of what your
 ## functions do
+
 
 ## Write a short comment describing this function
 
